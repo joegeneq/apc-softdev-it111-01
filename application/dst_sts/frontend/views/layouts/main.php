7e304b80c463ffa8e597152faaf77b404f13a097
Carl Samson <cbsamson16@gmail.com>
2015-03-08 16:35:53 +0800

    Updated indexes for backend starter pages.

--- application/dst_sts/frontend/views/layouts/main.php
+++ application/dst_sts/frontend/views/layouts/main.php
@@ -26,7 +26,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => 'D&rsquo;Carmelite School of Taguig',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
