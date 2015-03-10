d4ece137ede0f42bdad0dd6aa2a20d329d8b1c53
Carl Samson <cbsamson16@gmail.com>
2015-03-11 05:53:24 +0800

    Modified About page.

--- application/dst_sts/frontend/views/site/about.php
+++ application/dst_sts/frontend/views/site/about.php
@@ -6,9 +6,43 @@ $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
 
-    <code><?= __FILE__ ?></code>
+    <!--<code><?= __FILE__ ?></code>-->
+
+    <div class="row">
+    		<div class="col-xs-11">
+                <h3>Vision</h3>
+
+                <p style="font-size:16px; margin">We envision ourselves as one family committed to the integral formation of the learners and pursuing excellence through quality service.</p>
+            </div>
+
+            <div class="col-xs-11">
+                <h3>Mission</h3>
+
+
+                <p style="font-size:16px; margin">The D’ Carmelite School of Taguig was established to fulfil the following mission:</p>
+                <li>To provide a well-rounded system of education based on a LEARN AND TRAIN curriculum</li>
+                <li>Enhance the communication skills of a child particularly in English spoken as a second language, and in the National Language as well</li>
+                <li>Develop competency in the use and application of skills in Mathematics</li>
+                <li>Integration, into his total person, sound Christian values, and standards to morality</li>
+                <li>A deeply-seated appreciation and pride in his being a Filipino</li>
+            </div>
+
+            <div class="col-xs-11">
+                <h3>Address</h3>
+
+                <p style="font-size:16px; margin">Blk. 2, Lot 10, Phase I, EP Village, Western Bicutan, Taguig City, Metro Manila, Taguig, Metro Manila</p>
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15449.80548443234!2d121.049911!3d14.516155!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa675f948b7f7481b!2sD+Carmelite+School%2C+Inc.!5e0!3m2!1sen!2sph!4v1426023855716" width="600" height="450" frameborder="0" style="border:0"></iframe>
+            </div>
+
+            <div class="col-xs-11">
+                <h3>Contact Information</h3>
+
+                <p style="font-size:16px; margin">(02) 838 9697</p>
+
+            </div>
+        </div>
+
 </div>
