54c7f0b9a78e027705641e6e02f8ae2947198d79
Brandon Cruz <bltzkrg18@gmail.com>
2015-04-11 03:02:47 +0800

    Modified the form of adviser.

--- application/dst_sts/backend/views/adviser/_form.php
+++ application/dst_sts/backend/views/adviser/_form.php
@@ -12,13 +12,11 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id')->textInput() ?>
-
     <?= $form->field($model, 'adviser_first_name')->textInput(['maxlength' => 45]) ?>
 
     <?= $form->field($model, 'adviser_last_name')->textInput(['maxlength' => 45]) ?>
 
-    <?= $form->field($model, 'adviser_gender')->dropDownList([ 'Male' => 'Male', 'Female' => 'Female', ], ['prompt' => '']) ?>
+    <?= $form->field($model, 'adviser_gender')->dropDownList([ 'Male' => 'Male', 'Female' => 'Female', ], ['prompt' => 'Select gender']) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
