06ea624bdb459e190faadb401aca3d9748820ee4
Carl Samson <cbsamson16@gmail.com>
2015-04-11 10:14:22 +0800

    Added Pjax feature for quick search on "Parents" list.

--- application/dst_sts/backend/views/parents/index.php
+++ application/dst_sts/backend/views/parents/index.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\widgets\Pjax;
 
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\ParentsSearch */
@@ -19,6 +20,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Create Parents', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
+    <?php Pjax::begin(); ?>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
@@ -44,5 +46,6 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
+    <?php Pjax::end(); ?>
 
 </div>
