36b74088a53fe6e898a8fa006f4da59043428b0e
Brandon Cruz <bltzkrg18@gmail.com>
2015-04-22 12:03:35 +0800

    For hosting.

--- application/dst_sts/common/config/main-local.php
+++ application/dst_sts/common/config/main-local.php
@@ -4,8 +4,8 @@ return [
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=dst_sts',
-            'username' => 'root',
-            'password' => '',
+            'username' => 'dst_sts',
+            'password' => 'dst_sts',
             'charset' => 'utf8',
         ],
         'mailer' => [
